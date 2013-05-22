f8d1ea118e50e753d1eb3cad12f1b653054272aa
yohan rodrigues <valigar@MacBook-Pro-de-yohan.local>
2013-05-22 09:46:55 +0200

    ok

--- src/User/BlocNoteBundle/UserBlocNoteBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace User\BlocNoteBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class UserBlocNoteBundle extends Bundle
-{
-}
--- src/User/UserBundle/UserUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace User\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class UserUserBundle extends Bundle
-{
-}
